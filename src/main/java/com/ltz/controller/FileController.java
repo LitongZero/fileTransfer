3b5bd4b4d66a4d9e089e6c6b32e775c31a0698f9
li.tong <li.tong@digibms.com>
2023-04-18 10:08:05 +0800

    优化

--- src/main/java/com/ltz/controller/FileController.java
+++ src/main/java/com/ltz/controller/FileController.java
@@ -2,6 +2,7 @@ package com.ltz.controller;
 
 import com.ltz.entity.Myfile;
 import com.ltz.util.FileUtil;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,24 +12,31 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
-import java.io.*;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class FileController {
 
-    private static String UPLOAD_PATH = "./";
+    @Value("${upLoadPath:./}")
+    private String upLoadPath;
 
 
     @RequestMapping("/")
     public String file(Model model) {
-        File file = new File("./");
+        File file = new File(upLoadPath);
         File[] files = file.listFiles();
         List<Myfile> result = new ArrayList<>(files.length);
-        for (File f :
-                files) {
-            result.add(new Myfile(f.getName(), FileUtil.formatFileSize(f.length())));
+        for (File f : files) {
+            if (f.isDirectory()) {
+                result.add(new Myfile(f.getName(), "文件夹"));
+            } else {
+                result.add(new Myfile(f.getName(), FileUtil.formatFileSize(f.length())));
+            }
         }
         model.addAttribute("files", result);
         return "index";
@@ -36,38 +44,24 @@ public class FileController {
 
     @RequestMapping("/download/{fileName}")
     public String downLoad(Model model, HttpServletResponse response, @PathVariable("fileName") String fileName) {
-        File file = new File("./" + fileName);
+        File file = new File(new File(upLoadPath).getAbsolutePath(), fileName);
         if (!file.exists()) {
             model.addAttribute("msg", "下载失败");
-            return "/index";
+            return "index";
+        } else if (file.isDirectory()) {
+            model.addAttribute("msg", "文件夹不能直接下载");
+            return "index";
         } else {
             response.setContentType("application/force-download");
-
-            try {
-                BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
-
-                try {
-                    byte[] buff = new byte[1024];
-                    ServletOutputStream os = response.getOutputStream();
-
-                    int i;
-                    while ((i = bis.read(buff)) != -1) {
-                        os.write(buff, 0, i);
-                        os.flush();
-                    }
-                    os.close();
-                } catch (Throwable throwable) {
-                    try {
-                        bis.close();
-                    } catch (Throwable var9) {
-                        throwable.addSuppressed(var9);
-                    }
-
-                    throw throwable;
+            try (BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file))) {
+                byte[] buff = new byte[1024];
+                int len;
+                ServletOutputStream os = response.getOutputStream();
+                while ((len = bis.read(buff)) != -1) {
+                    os.write(buff, 0, len);
+                    os.flush();
                 }
-
-                bis.close();
-            } catch (IOException var11) {
+            } catch (IOException e) {
                 model.addAttribute("msg", "下载失败");
                 return "index";
             }
@@ -77,27 +71,24 @@ public class FileController {
         }
     }
 
-    @RequestMapping("/upload")
+    @RequestMapping({"/upload"})
     public String upLoad(Model model, @RequestParam("file") MultipartFile file) {
         if (file.isEmpty()) {
             model.addAttribute("msg", "还未选择任何文件");
             return "/index";
         } else {
-            String name = file.getOriginalFilename();
-
             try {
-                FileOutputStream fos = new FileOutputStream(UPLOAD_PATH + name);
-                fos.write(file.getBytes());
-                fos.close();
-                System.out.println(file.getOriginalFilename() + "上传成功");
+                String name = file.getOriginalFilename();
+                File saveFile = new File(new File(upLoadPath).getAbsolutePath(), name);
+                file.transferTo(saveFile);
+                System.out.println(file.getOriginalFilename() + "上传成功" + saveFile.getPath());
             } catch (IOException e) {
-                System.out.println(file.getOriginalFilename() + "保存失败");
                 e.printStackTrace();
+                System.out.println(file.getOriginalFilename() + "保存失败");
             }
 
             model.addAttribute("msg", "上传成功");
             return "redirect:/";
         }
     }
-
 }
